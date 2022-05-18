a10f9c0d2ddf730748184416d3c1bad9cb4cf0c5
Aparna-devi-chinnu-R <aparnadevi.rajavel@thoughtworks.com>
2022-05-18 18:17:43 +0530

    [Aparna] Add . extra configuration for xml.

--- src/main/java/com/example/springCore/Product.java
+++ src/main/java/com/example/springCore/Product.java
@@ -1,6 +1,21 @@
 package com.example.springCore;
 
+import java.beans.ConstructorProperties;
+
 public class Product {
+
+    private String productName;
+
+    public Product() {
+    }
+
+    public Product(String productName) {  // this name should match with name in xml file constructor-arg
+        super();
+        this.productName = productName;
+    }
+
+
+
     public void printProductName(String name, String price){
         System.out.println("The product " +name + "has a price " +price);
     }
