72ad23eb0076fceeaae058d03061b295073bc1f0
Aparna-devi-chinnu-R <aparnadevi.rajavel@thoughtworks.com>
2022-05-18 16:55:20 +0530

    [Aparna] Add . xml configuration.

---
+++ src/main/java/com/example/springCore/Product.java
@@ -0,0 +1,7 @@
+package com.example.springCore;
+
+public class Product {
+    public void printProductName(String name, String price){
+        System.out.println("The product " +name + "has a price " +price);
+    }
+}
---
+++ src/main/java/com/example/springCore/SpringCoreApplication.java
@@ -0,0 +1,28 @@
+package com.example.springCore;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+@SpringBootApplication
+public class SpringCoreApplication {
+
+	public static void main(String[] args) {
+
+		// deprecated way
+		BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("springConfig.xml"));
+		Product product = beanFactory.getBean("product", Product.class);
+
+		//correct way , return application context , can you also use BeanFactory instead of application context
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("springConfig.xml");
+		Product product1 = applicationContext.getBean("product", Product.class);
+
+	}
+
+
+
+}
