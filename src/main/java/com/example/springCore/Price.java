900a76abb55bef6e181ab57ba67401fdcf017cfb
Aparna-devi-chinnu-R <aparnadevi.rajavel@thoughtworks.com>
2022-05-18 22:37:55 +0530

    XML Configuration | Setter based injection.'

---
+++ src/main/java/com/example/springCore/Price.java
@@ -0,0 +1,23 @@
+package com.example.springCore;
+
+public class Price {
+
+    private int amount;
+    private String currencyType;
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getCurrencyType() {
+        return currencyType;
+    }
+
+    public void setCurrencyType(String currencyType) {
+        this.currencyType = currencyType;
+    }
+}
